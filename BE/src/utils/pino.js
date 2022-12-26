16825ac70d77c393b9f564c7da2240bd852a3a8f
상준 안 <ahosang0056@gmail.com>
2022-12-26 22:54:56 +0900

    chore: pino env 설정 추가

--- BE/src/utils/pino.js
+++ BE/src/utils/pino.js
@@ -3,4 +3,4 @@ const pretty = require("pino-pretty");
 const stream = pretty({
   colorize: true,
 });
-exports.logger = pino({ level: "info" }, stream);
+exports.logger = pino({ level: process.env.LOG_LEVEL || "info" }, stream);
